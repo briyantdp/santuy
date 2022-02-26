1f7f17f60fc152b7242fec9f8e9323b3c86682f3
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-26 07:57:05 +0700

    Add 404 page, and refactor Router based on react-router 5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+
+
 import LandingPage from "./pages/LandingPage";
 import DetailPage from "./pages/DetailPage";
 import Checkout from "./pages/Checkout";
-import "./assets/scss/style.scss";
 import Example from "pages/Example";
+import NotFound from "pages/404";
+
+import "./assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/properties/:id" element={<DetailPage />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/example" element={<Example />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/properties/:id" component={DetailPage} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/example" component={Example} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
