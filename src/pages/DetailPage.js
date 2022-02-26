21e7318af075de0ede1aebcdc3fda40d21b442e9
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-26 08:06:14 +0700

    Add animation and redux functionality

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import { connect } from "react-redux";
 
 import Header from "../parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
@@ -9,15 +11,28 @@ import SimilarRooms from "parts/SimilarRooms";
 import Testimonial from "parts/Testimonial";
 import Footer from "../parts/Footer";
 
-import itemDetails from "../json/itemDetails.json";
+// import itemDetails from "../json/itemDetails.json";
+
+import { checkoutBooking } from "stores/actions/checkout";
+import { fetchPage } from "stores/actions/page";
+
 
 class DetailPage extends Component {
   componentDidMount() {
-    document.title = "Santuy | Home";
     window.scroll(0, 0);
+    
+    if(!this.props.page[this.props.match.params.id]);
+      this.props.fetchPage(`/detail-page/${this.props.match.params.id}`, this.props.match.params.id)
+      .then((response) => {
+        document.title = `Santuy | ${response.title}`
+      })
   }
 
   render() {
+    const { page, match, checkoutBooking} = this.props;
+
+    if(!page[match.params.id]) return null;
+
     const breadcrumb = [
       { pageTitle: "Home", pageHref: "" },
       { pageTitle: "House Details", pageHref: "" },
@@ -26,24 +41,32 @@ class DetailPage extends Component {
     return (
       <>
         <Header />
-        <PageDetailTitle data={itemDetails} breadcrumb={breadcrumb} />
-        <PageDetailImages data={itemDetails.imageUrl} />
+        <PageDetailTitle data={page[match.params.id]} breadcrumb={breadcrumb} />
+        <PageDetailImages data={page[match.params.id].imageId} />
         <div className="container">
           <div className="row">
             <div className="col-md-6">
-              <PageDetailDescription data={itemDetails} />
+              <Fade bottom>
+                <PageDetailDescription data={page[match.params.id]} />
+              </Fade>
             </div>
-            <div className="col-md-5 offset-md-1">
-              <BookingForm itemDetails={itemDetails} />
-            </div>
+            <Fade bottom>
+              <div className="col-md-5 offset-md-1">
+                <BookingForm dataBooking={page[match.params.id]} startBooking={checkoutBooking} />
+              </div>
+            </Fade>
           </div>
         </div>
-        <SimilarRooms data={itemDetails.similar} />
-        <Testimonial data={itemDetails.testimonial} />
+        <SimilarRooms data={page[match.params.id].similarRoomsId} />
+        <Testimonial data={page[match.params.id].testimonial} />
         <Footer />
       </>
     );
   }
 }
 
-export default DetailPage;
+const mapStateToProps = (state) => ({
+  page : state.page,
+})
+
+export default connect(mapStateToProps, {fetchPage, checkoutBooking})(DetailPage);
