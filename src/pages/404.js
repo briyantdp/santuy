ed894e639a24898f9c63c2d1e469062da7ca1d6c
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-26 17:18:47 +0700

    Refactor 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ function NotFound(props) {
         className="row align-items-center justify-content-center text-center"
         style={{ height: "100vh" }}
       >
-        <div className="col-4">
+        <div className="col-lg-6 col-md-8 col-sm-10">
           <h1 className="h1 font-weight-bold">Are you lost ?</h1>
           <p className="pt-4">
             Some page are still in development, maybe you can go back if you
