bfe0e4cd0cb74b2c925d3a224307b793f1c089ed
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-26 07:58:32 +0700

    Add errorAlert with toastify library

---
+++ src/configs/axios/errorResponseHandler.js
@@ -0,0 +1,15 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function errorResponseHandler (error) {
+    if (error) {
+        let message;
+        if (error.response) {
+            if (error.response.status === 500)  message = "Something went wrong !";
+            else message = error.response.message
+            console.log(message);
+            toast(message);
+            return Promise.reject(error);
+        }
+    }
+}
