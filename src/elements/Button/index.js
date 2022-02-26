45dbab9fc686060aa1e63272b48142d3b81febb3
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-26 07:59:06 +0700

    Add isLight props

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -7,6 +7,7 @@ import propTypes from "prop-types";
 export default function Button(props) {
   const className = [props.className];
   if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
   if (props.isSmall) className.push("btn-sm");
   if (props.isLarge) className.push("btn-lg");
   if (props.isBlock) className.push("btn-block");
@@ -79,6 +80,7 @@ Button.propTypes = {
   isLarge: propTypes.bool,
   hasShadow: propTypes.bool,
   isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
   isBlock: propTypes.bool,
   isExternal: propTypes.bool,
 };
